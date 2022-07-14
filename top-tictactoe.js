3ff653eb80baeaa4d8c25ff792a4c760c60e3d7f
Anthony <aajohnso.coms@gmail.com>
2022-07-13 22:51:15 -0600

    add js function logic to determine winner

--- top-tictactoe.js
+++ top-tictactoe.js
@@ -1,13 +1,37 @@
 const gameBoard = (() => {
     
     const board = ["","","","","","","","",""];
+
+    const winningConditions = [
+        ["0","1","2"],
+        ["3","4","5"],
+        ["6","7","8"],
+        ["0","3","6"],
+        ["1","4","7"],
+        ["2","5","8"],
+        ["0","4","8"],
+        ["2","4","6"]
+    ];
     
     const Player = (name, mark) => {
+        const cellsPlayed = [];
         const getName = () => name;
         const addMark = (cell) => {
             board[cell] = mark;
+            cellsPlayed.push(cell);
         }
-        return {addMark, getName};
+        const checkWin = () => {
+            let win = false;
+            winningConditions.forEach(
+                winCondition => {
+                    if(winCondition.every(cell => cellsPlayed.includes(cell))) {
+                        win = true;
+                    }
+                }
+            );
+            return win;
+        }
+        return {addMark, getName, checkWin};
     };
 
     const playerOne = Player("Player One", "x");
@@ -19,6 +43,8 @@ const gameBoard = (() => {
 
 const displayController = (() => {
 
+    let turn = 0;
+    let gameOver = false;
     let currentTurn = gameBoard.playerOne;
     
     const gameBoardDiv = document.getElementById("gameboard");
@@ -29,13 +55,27 @@ const displayController = (() => {
     }
 
     function displayMark() {
+        turn++;
         let gameCell = this.getAttribute("game-cell");
         currentTurn.addMark(gameCell);
         this.innerText = gameBoard.board[gameCell];
         this.classList.remove("open");
         this.classList.add("closed");
         this.removeEventListener("click", displayMark);
-        changeTurn();
+        if (turn == 9) {
+            gameOver = true;
+            console.log("TIE");
+        } else if (currentTurn.checkWin()) {
+            gameOver = true;
+            console.log("WIN");
+            for (i = 0; i < gameBoardCells.length; i++) {
+                gameBoardCells[i].removeEventListener("click", displayMark);
+                gameBoardCells[i].classList.remove("open");
+                gameBoardCells[i].classList.add("closed");
+            }
+        } else {
+            changeTurn();
+        }
     }
 
     const changeTurn = () => {
